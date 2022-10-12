5e2a4583c3a58a2d5fc3f0547035ce102aa56f88
Marcela Kramer <marcelabarretokramer10@gmail.com>
2022-10-11 22:15:02 -0300

    desenvolvendo parte do html/css e testando código javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const api = axios.create({
+/* const api = axios.create({
     baseURL:
         "https://api.weatherapi.com/v1/current.json?key=ee51adc00b584f8fb65201705220310&q=",
 });
@@ -18,4 +18,23 @@ async function getWeather() {
         .catch((err) => {
             console.log(err);
         });
+} */
+
+
+async function getWeather() {
+    const city = document.querySelector('#search').value
+
+    const url = `https://api.weatherapi.com/v1/current.json?key=ee51adc00b584f8fb65201705220310&q=${city}`
+
+    const response = await fetch(url)
+
+    const data = await response.json()
+
+    const temp = document.querySelector('#temperature')
+    const newCity = document.querySelector('#city')
+
+    console.log(data)
+
+    temp.innerText = data.current.temp_c + 'º'
+    newCity.innerText = (data.location.name).toUpperCase()
 }

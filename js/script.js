327c15fd3b0223641c8eae53913fd3c08d2271f2
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2022-10-12 13:49:20 -0300

    atualizando o painel principal
    mudei o onclick pra um listener

--- js/script.js
+++ js/script.js
@@ -1,53 +1,23 @@
-/* const api = axios.create({
-    baseURL:
-        "https://api.weatherapi.com/v1/current.json?key=ee51adc00b584f8fb65201705220310&q=",
-});
-
-async function getWeather() {
-    const p = document.getElementById("clima");
-    const cidade = document.getElementById("cidade").value
-
-    await api
-        .get(`/${cidade}`)
-        .then((response) => {
-            const a = `${response.data.current.temp_c}`;
-            console.log(response.data)
-
-            p.innerText = a;
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-} */
 import { cities } from "./cities.js";
 
-async function getWeather() {
-    const city = document.querySelector('#search').value || 'London'
+// CRUD
+const getWeather = async () => {
+    const city = document.querySelector("#search").value || "London";
 
-    const url = `https://api.weatherapi.com/v1/current.json?key=ee51adc00b584f8fb65201705220310&q=${city}`
+    const url = `https://api.weatherapi.com/v1/current.json?key=ee51adc00b584f8fb65201705220310&q=${city}`;
 
-    const response = await fetch(url)
+    const response = await fetch(url);
 
-    const data = await response.json()
+    const data = await response.json();
 
-    const temp = document.querySelector('#temperature')
-    const newCity = document.querySelector('#city')
+    const temp = document.querySelector("#temperature");
+    const newCity = document.querySelector("#city");
 
-    console.log(data)
+    console.log(data);
 
-    temp.innerText = data.current.temp_c + 'ºC'
-    newCity.innerText = (data.location.name).toUpperCase()
-}
-
-
-
-
-
-
-
-
-
-
+    temp.innerText = data.current.temp_c + "ºC";
+    newCity.innerText = city.toUpperCase();
+};
 
 // Funções dos Cards
 async function requestData(city) {
@@ -56,27 +26,27 @@ async function requestData(city) {
 
     const data = await response.json();
 
-    const cityContainer = document.getElementById('cities-container')
-
-
+    const cityContainer = document.getElementById("cities-container");
 
     const cardView = `
         <div> 
             ${data.location.name}
             ${data.current.temp_c}
         </div>
-    `
+    `;
 
-
-    cityContainer.insertAdjacentHTML('beforeend', cardView)
+    cityContainer.insertAdjacentHTML("beforeend", cardView);
 }
 
 async function loadCards() {
     for (let city of cities) {
-        requestData(city)
+        requestData(city);
     }
 }
 
-
+// Listeners
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", getWeather);
 
 loadCards();
+getWeather();

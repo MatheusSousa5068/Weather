58c1643a1a615fcac89ec02ac91a8a2712fe31d3
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2022-10-12 19:04:36 -0300

    formatando o código

--- js/script.js
+++ js/script.js
@@ -1,30 +1,22 @@
 import { cities } from "./utils/cities.js";
 
-const searchBtn = document.getElementById("search-btn");
-
-
-import { requestData } from "./utils/api.js";
 import { createCard } from "./views/cards.js";
 import { getWeather } from "./views/mainCard.js";
 
+const searchBtn = document.getElementById("search-btn");
+
 async function loadCards() {
     cities.map(createCard);
-};
-
+}
 
 // Listeners
 searchBtn.addEventListener("click", getWeather);
 
 document.addEventListener("keydown", function (event) {
-    if (event.key == "Escape") {
-        document.querySelector("#search").innerHTML = ''
-    } else if (event.key == "Enter") {
+    if (event.key == "Enter") {
         getWeather();
     }
 });
-
-
-
 
 // Chamadas de funções
 loadCards();

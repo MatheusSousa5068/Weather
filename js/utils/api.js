8c13fe2a0407a0a06a856f035e963655e91b46be
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2022-10-31 10:26:28 -0300

    alterando key

--- js/utils/api.js
+++ js/utils/api.js
@@ -1,5 +1,5 @@
 const requestData = async (city) => {
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&include=current&key=7BYA5PVEB78U8CP453KSNKUVJ&contentType=json`;
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&include=current&key=SMSLCV9DMP7FSRJX66WBSN4BU&contentType=json`;
     const response = await fetch(url);
     const data = await response.json();
 
